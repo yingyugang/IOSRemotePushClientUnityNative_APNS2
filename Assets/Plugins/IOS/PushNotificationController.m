23afd02a8034ccf02f08dc2479ea2aef14c4a423
DESKTOP-OSSCN77\yingy <yingyugang@gmail.com>
2021-04-30 10:52:22 +0900

    Retrieve push notification.

--- Assets/Plugins/IOS/PushNotificationController.m
+++ Assets/Plugins/IOS/PushNotificationController.m
@@ -50,11 +50,18 @@
 - (BOOL)WechatSignInAppController:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     NSLog(@"当程序载入后执行");
-    
-    //UIRemoteNotificationType myTypes = UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound;
-    //   [[UIApplication sharedApplication] registerForRemoteNotificationTypes:myTypes];
-    [[UIApplication sharedApplication]  registerForRemoteNotifications];
-  return  [self WechatSignInAppController:application
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
+
+
+    NSDictionary *remoteNotif = [launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
+	if (remoteNotif) {
+    	NSData *infoData = [NSJSONSerialization dataWithJSONObject:remoteNotif options:0 error:nil];
+    	NSString *info = [[NSString alloc] initWithData:infoData encoding:NSUTF8StringEncoding];
+    	[self.windowRootController displayNotification:[NSString stringWithFormat:@"From didFinishLaunch: %@", info]];
+    	[UIApplication sharedApplication].applicationIconBadgeNumber -= 1;
+	}
+
+    return  [self WechatSignInAppController:application
             didFinishLaunchingWithOptions:launchOptions];
 }
 
@@ -97,6 +104,17 @@
     NSLog(@"Remote notification support is unavailable due to error: %@", err);  
 }
 
+- (void)application:(UIApplication *)application 
+        didReceiveRemoteNotification:(nonnull NSDictionary *)userInfo 
+        fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler {
+   
+    NSData *infoData = [NSJSONSerialization dataWithJSONObject:userInfo options:0 error:nil];
+    NSString *info = [[NSString alloc] initWithData:infoData encoding:NSUTF8StringEncoding];
+     NSLog(@"%@",info);
+    [self.windowRootController displayNotification:[NSString stringWithFormat:@"From didReceiveRemoteNotification: %@", info]];
+    // 这里将角标数量减一，注意系统不会帮助我们处理角标数量
+    application.applicationIconBadgeNumber = notification.applicationIconBadgeNumber - 1;
+}
 
 - (NSString *)fetchDeviceToken:(NSData *)deviceToken {
     NSUInteger len = deviceToken.length;
@@ -110,4 +128,20 @@
     }
     return [hexString copy];
 }
+
+- (void)registerRemoteNotifications {
+    // 区分是否是 iOS8 or later
+    if ([[UIApplication sharedApplication] respondsToSelector:@selector(registerForRemoteNotifications)]) {
+        // 这里 types 可以自定义，如果 types 为 0，那么所有的用户通知均会静默的接收，系统不会给用户任何提示(当然，App 可以自己处理并给出提示)
+        UIUserNotificationType types = (UIUserNotificationType) (UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert);
+        // 这里 categories 可暂不深入，本文后面会详细讲解。
+        UIUserNotificationSettings *mySettings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+        // 当应用安装后第一次调用该方法时，系统会弹窗提示用户是否允许接收通知
+        [[UIApplication sharedApplication] registerUserNotificationSettings:mySettings];
+    } else {
+        UIRemoteNotificationType types = UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound;
+        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:types];
+    }
+}
+
 @end
